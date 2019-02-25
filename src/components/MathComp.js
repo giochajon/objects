99e10f590c928279672e15b99d4f875a90a7247a
Giovanni Chajon <giochajon@gmail.com>
2019-02-25 13:59:33 -0700

    first commit of the objects project

---
+++ src/components/MathComp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class MathComp extends React.Component {
+
+	render() {
+		return (<h1> Hello world from MathComp class </h1> );
+	}
+
+
+}
+
+export default  MathComp 
---
+++ src/components/math.js
@@ -0,0 +1,23 @@
+function sum(a,b) {
+	const c = (a+b);
+	return c; 
+}
+
+function substract(a,b) {
+	const d = (a-b);
+	return d; 
+}
+
+function multiply(a,b) {
+	const e = (a*b);
+	return e; 
+}
+
+function divide(a,b) {
+	const f = (a/b);
+	return f; 
+	return 0; 
+}
+
+
+export default { sum,substract,multiply,divide };
