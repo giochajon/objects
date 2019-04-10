9fb8099191fb9ec42877d5ecc0200c93c4100803
Giovanni Chajon <giochajon@gmail.com>
2019-04-09 22:51:39 -0600

    created the new interface for accounts controller

--- src/components/AccountControllerInterface.js
+++ src/components/AccountControllerInterface.js
@@ -7,17 +7,116 @@ class AccountInterface extends React.Component {
         super(props);
 
         this.state = {
-           
+        newAccountName :null,  
+        newAccountBalance :null,
+        clicks : 0
         };
         this.controller = new AccountController()
+
     }
+
+
+	addNewAccount = () => {
+		let newAcc = new Account (this.state.newAccountName,this.state.newAccountBalance)
+		this.controller.addAccount(newAcc) 
+		this.setState({ newAccountName: "" });
+		this.setState({ newAccountBalance: "" });
+		console.log(this.controller.accountList)
+		//this.forceUpdate()
+
+
+	}
+
+	handleChangeName = (event) => {
+		this.setState({ newAccountName: event.target.value });
+	}
+
+	handleChangeBalance = (event) => {
+		this.setState({ newAccountBalance: event.target.value });
+	}
+
+	deleteAccount = (who) => {
+		this.controller.deleteAccount(who)
+
+	let a = this.state.clicks
+	this.setState({clicks: a + 1 });
+	//alert (who +" "+ a)
+	}
+
+	renameAccount = (who) => {
+		let newName = prompt("enter the new name the "+who+ " account")
+		this.controller.renameAccount(who,newName)
+        
+	let a = this.state.clicks
+	this.setState({clicks: a + 1 });
+	//alert (who +" "+ a)
+	}    
+	
+
+		showData = () => {
+        let lista = [];
+
+        const arr = this.controller.accountList;
+        arr.forEach((element) => {
+         lista.push(
+                <div className="Acc">
+                 <p>Name: {element.name}</p> <p> Balance:  {element.balance}</p>
+				 
+				 <button onClick={() => {
+                        //this.props.funcMoveIn(this.props.name);
+                       //alert('miau')
+                       this.renameAccount(element.name)
+                    }}
+              >Rename Account</button>
+              <button onClick={() => {
+                        //this.props.funcMoveIn(this.props.name);
+                       //alert('miau')
+                       this.deleteAccount(element.name)
+                    }}
+              >Delete Account</button>
+
+				 </div>
+            );
+        });
+		return lista	
+	}
 
     render() {
         
+        let accountCount = this.controller.accountList.length
+         
 
         return (
+        	<div>
+        	<p>Number of accounts{accountCount}</p>
+        	<p>total: {this.controller.total}   highest: {this.controller.highest}   lowest: {this.controller.lowest} </p>
+            
             <div className="Acc">
-                
+             <p>New Account: </p> 
+              <input
+                    type="text"
+                    value={this.state.newAccountName}
+                    onChange={this.handleChangeName}
+                    placeholder="Name"
+                /> 
+                <input
+                    type="text"
+                    value={this.state.newAccountBalance}
+                    onChange={this.handleChangeBalance}
+                    placeholder="Inital Balance"
+                />
+              <button onClick={() => {
+                        //this.props.funcMoveIn(this.props.name);
+                       //alert('miau')
+                       this.addNewAccount()
+                    }}
+              >Add Account</button>
+            
+            </div>
+            {this.showData()}
+           
+            
+
             </div>
         );
     }
